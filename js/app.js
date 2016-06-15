2eed70e786a56af45bb4ba694d301840584fa6dc
Eliseev Alexandr <sansurf@inbox.ru>
2016-06-15 13:09:03 +0500

    Добавил стили. В app.js так и не понял, как кнопку нужную выбирать

--- js/app.js
+++ js/app.js
@@ -1,36 +1,68 @@
 var link = [
 	{
 		title: "Посмотреть трейлер",
-		src: ""
+		href: "#1"
 	},
 	{
 		title: "Показать ещё",
-		src: ""
+		href: "#2"
 	},
 	{
 		title: "Это всё не то",
-		src: ""
+		href: "#3"
 	},
 	{
 		title: "Что ещё посмотреть",
-		src: ""
+		href: "#4"
 	}		
 ];
 
 var Button = React.createClass({
+	propTypes: {
+		link: React.PropTypes.shape ({
+			title: React.PropTypes.string.isRequired,
+			href: React.PropTypes.string.isRequired
+		})
+	},
+
 	render: function() {
-		var button = this.props.link;
-		var buttonTemplate = button.map(function(item, index) {
-				return (
-					<div key={index}>
-						<p className="oneButton">{item.title}</p>
-					</div>
-				)
+		var title = this.props.data.title,
+			href = this.props.data.href;
+
+		var butSet = this.props.data.map(function(item, index) {
+			return (
+				<div key={index}>
+					<a href={item.href}>{item.title}</a>
+				</div>
+			)
 		});
-				
+
+		// this.props.data.some(function(item, index) {
+		// 	return (
+		// 		<div key={index}>
+		// 			<a href={item.href}>{item.title}</a>
+		// 		</div>
+		// 	)
+		// });
+
+		// switch(title) {
+		// 		case "Посмотреть трейлер":
+		// 		return href = "#1";
+		// 		break;
+		// 		case "Показать ещё":
+		// 		return href = "#2";
+		// 		break;
+		// 		case "Это всё не то":
+		// 		return href = "#3";
+		// 		break;
+		// 		case "Что ещё посмотреть":
+		// 		return href = "#4";
+		// 		break;
+		// 	}
+
 		return (
 			<div className="button">
-				{buttonTemplate}
+				{butSet}
 			</div>
 		)
 	}
@@ -39,6 +71,6 @@ var Button = React.createClass({
 
 ReactDOM.render(
 
-	<Button link = {link} />,
+	<Button data={link} />,
 	document.getElementById('button')
 );
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use('/', express.static(_dirname));
+app.use('/', express.static(__dirname));
 app.listen(app.get('port'), function() {
 	console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
